c61b177e5881180d18716137a8c84487ab7d63d2
rassx6 <rass06@mail.ru>
2024-03-04 18:30:44 +0600

    add_01

--- testcryptoapp/lib/repositories/crypto_coins/crypto_coins_repository.dart
+++ testcryptoapp/lib/repositories/crypto_coins/crypto_coins_repository.dart
@@ -4,14 +4,15 @@ import 'package:testcryptoapp/repositories/crypto_coins/models/crypto_coin.dart'
 class CryptoCoinsRepository {
   Future<List<CryptoCoin>> getCoinsList() async {
     final response = await Dio().get(
-      'https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,BNB&tsyms=USD'
+      'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BNB&tsyms=USD'
     );
     final data = response.data as Map<String, dynamic>;
-    final cryptoCoinsList = data.entries
-    .map((e) => CryptoCoin(
+    final dataRaw = data['RAW'] as Map<String, dynamic>;
+    final cryptoCoinsList = dataRaw.entries
+    .map((e) { CryptoCoin(
       name: e.key, 
       priceInUSD: (e.value as Map<String, dynamic>)['USD']),
-      )
+      )}
     .toList();
     return cryptoCoinsList;
   }
